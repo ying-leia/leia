e4379b8d1106a6f88dede68c8df74202623b74b5
Ying Han Yeo <ying@leiaflora.com>
2025-06-16 18:18:34 +0100

    redeploying for sign in

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import Link from "next/link";
 import Navigation from '../../components/Navigation';
 
 export default function SignInRegister() {
