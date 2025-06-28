6941510b199bf528d9495d1ef1e45ca1fd84a235
Ying Han Yeo <ying@leiaflora.com>
2025-06-28 13:45:13 +0100

    Fix ESLint error: remove unused useState import from sign-in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Navigation from '../../components/Navigation';
 
 export default function SignInRegister() {
