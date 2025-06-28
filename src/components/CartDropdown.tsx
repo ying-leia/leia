9a564597a4c1565c81d5fc0cd037bab48e52ebad
Ying Han Yeo <ying@leiaflora.com>
2025-06-28 13:48:40 +0100

    Fix TypeScript error: add props interface to CartDropdown component

--- src/components/CartDropdown.tsx
+++ src/components/CartDropdown.tsx
@@ -1,3 +1,8 @@
-export default function CartDropdown() {
+interface CartDropdownProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function CartDropdown({ isOpen, onClose }: CartDropdownProps) {
   return <div>Cart functionality is now handled by Shopify integration.</div>;
 } 
