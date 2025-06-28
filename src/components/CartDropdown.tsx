64056a18dad5f7280c0eab126e4845b5a11f8cbb
Ying Han Yeo <ying@leiaflora.com>
2025-06-28 13:51:06 +0100

    Fix ESLint error: prefix unused props with underscores in CartDropdown

--- src/components/CartDropdown.tsx
+++ src/components/CartDropdown.tsx
@@ -3,6 +3,6 @@ interface CartDropdownProps {
   onClose: () => void;
 }
 
-export default function CartDropdown({ isOpen, onClose }: CartDropdownProps) {
+export default function CartDropdown({ isOpen: _isOpen, onClose: _onClose }: CartDropdownProps) {
   return <div>Cart functionality is now handled by Shopify integration.</div>;
 } 
